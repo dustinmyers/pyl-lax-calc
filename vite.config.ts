4c2a3299aaecff11838eead39ec343f5db7c40c0
Dustin Myers <dustin@desertmountain.io>
2023-11-01 22:09:53 -0600

    Rollup options

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,30 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    open: true,
+    hmr: {
+      overlay: true,
+    },
+  },
   plugins: [
     react(),
     cssInjectedByJsPlugin(),
   ],
   build: {
     cssCodeSplit: false,
+    outDir: 'dist',
     rollupOptions: {
       input: {
         app: './src/main.tsx',
+        main: resolve(__dirname, 'index.html'),
       },
     },
-    sourcemap: 'inline'
+    sourcemap: true
   },
 })
